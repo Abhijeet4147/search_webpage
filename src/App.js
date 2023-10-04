15b322dfacd546d9bd7d5bf637fb4e90beb93190
Abhijeet <abhijeetmundhe55@gmail.com>
2023-10-04 12:36:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CarSearch from './components/CarSearch';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/page/:page" element={<CarSearch />} />
+          <Route path="/" element={<CarSearch />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
